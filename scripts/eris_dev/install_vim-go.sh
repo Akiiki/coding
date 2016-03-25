4dd72674128e7bc32f8f2babd97587d9842b2b3c
Zach Ramsay <zach@erisindustries.com>
2016-03-25 16:17:42 -0700

    rm scripts until complete

--- scripts/eris_dev/install_vim-go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-# set -e
-
-sudo apt-get install vim-gocomplete gocode vim-syntax-go -y
-vim-addon-manager install go-syntax
-vim-addon-manager install gocode
-
-cp -r vim/ ~/.vim/
-cp vimrc ~/.vimrc
